addd72ca9aae49c9028c6991ed5883f04779b65a
RemyMozelle <timoz974@hotmail.fr>
2018-01-09 15:30:04 +0400

    go

--- app/models/Menu.js
+++ app/models/Menu.js
@@ -7,7 +7,6 @@ class Menu {
     return new Promise((resolve, reject) => {
       this.db.getPool().getConnection((err, connection) => {
         if(err) throw err;
-        console.log(connection);
         connection.query('SELECT * FROM menus', (error, results, fields) => {
           error ? reject(error) : resolve(results)
           connection.release();
