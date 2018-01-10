3cff8e9b1d0fad7b1bc887a51eb70dcdb2600794
RemyMozelle <timoz974@hotmail.fr>
2018-01-10 07:27:21 +0400

    pas encore trouvé le problème

--- config/routes/route.js
+++ config/routes/route.js
@@ -4,8 +4,12 @@ const route = express.Router();
 const appController = require('../../app/controllers/AppController');
 const cards = require('../../app/models/Cards');
 
-route.get('/', appController.portfolio);
+/* route.get('/', appController.portfolio);
 route.get('/cv/:cv', appController.getCv);
-route.post('/', appController.email);
+route.post('/', appController.email); */
+
+route.get('/', (req, res) => {
+  res.render('pages/index.ejs');
+})
 
 module.exports = route;
