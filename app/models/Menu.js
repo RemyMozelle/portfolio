bf0fbdc4c6eb7043f1db564d842d09ae6f3c0de2
RemyMozelle <timoz974@hotmail.fr>
2018-01-10 08:13:03 +0400

    check

--- app/models/Menu.js
+++ app/models/Menu.js
@@ -8,16 +8,17 @@ class Menu {
       this.db.getPool().query('SELECT name, url FROM menus', (error, results, fields) => {
         console.log(results);
         error ? reject(error) : resolve(results)
-      /* this.db.getPool().getConnection((err, connection) => {
-        console.log(connection, 'connection !');
-        if(err) throw err;
-        connection.query('SELECT name, url FROM menus', (error, results, fields) => {
-          error ? reject(error) : resolve(results)
-          connection.release();
-        });
-      }) */
-    });
-  })
+        /* this.db.getPool().getConnection((err, connection) => {
+          console.log(connection, 'connection !');
+          if(err) throw err;
+          connection.query('SELECT name, url FROM menus', (error, results, fields) => {
+            error ? reject(error) : resolve(results)
+            connection.release();
+          });
+        }) */
+      });
+    })
+  }
 }
 
 module.exports = new Menu();
