9baa2960fade5e6caa4522e882d864bdc6722386
RemyMozelle <timoz974@hotmail.fr>
2018-01-10 07:44:09 +0400

    check

--- app/models/Menu.js
+++ app/models/Menu.js
@@ -5,14 +5,17 @@ class Menu {
 
   getMenu() {
     return new Promise((resolve, reject) => {
-      this.db.getPool().getConnection((err, connection) => {
+      this.db.getPool().query('SELECT name, url FROM menus', (error, results, fields) => {
+        console.log(results);
+        error ? reject(error) : resolve(results)
+      /* this.db.getPool().getConnection((err, connection) => {
         console.log(connection, 'connection !');
         if(err) throw err;
         connection.query('SELECT name, url FROM menus', (error, results, fields) => {
           error ? reject(error) : resolve(results)
           connection.release();
         });
-      })
+      }) */
     });
   }
 }
