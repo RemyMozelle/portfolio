f4a481ddee84cfe839d252f2c997212906d3519f
RemyMozelle <timoz974@hotmail.fr>
2018-01-10 08:31:02 +0400

    gogogo heroku plz

--- config/database/Db.js
+++ config/database/Db.js
@@ -3,30 +3,17 @@ class Db {
   constructor() {
     this.mysql = require('mysql');
   }
-  getPool(prod = true) {
-    if (prod == true) {
-      console.log('PASSE en prod');
-      const pool = this.mysql.createPool({
-        connectionLimit: 10,
-        host: process.env.HOSTP,
-        user: process.env.USERNAMEP,
-        password: process.env.PASSWORDP,
-        database: process.env.DATABASEP,
-      });
+  getPool() {
 
-      return pool
-    } else {
-      console.log('passe en dev');
-      const pool = this.mysql.createPool({
-        connectionLimit: 10,
-        host: process.env.HOSTDEV,
-        user: process.env.USERNAMEDEV,
-        password: process.env.PASSWORDDEV,
-        database: process.env.DATABASEDEV
-      });
+    const pool = this.mysql.createPool({
+      connectionLimit: 10,
+      host: process.env.HOSTP,
+      user: process.env.USERNAMEP,
+      password: process.env.PASSWORDP,
+      database: process.env.DATABASEP,
+    });
 
-      return pool
-    }
+    return pool
   }
 }
 
