cda987abd8573db7b7be8e269392a73d2c66182a
RemyMozelle <timoz974@hotmail.fr>
2018-01-10 09:38:55 +0400

    nouvelle db jaws

--- config/database/Db.js
+++ config/database/Db.js
@@ -5,13 +5,15 @@ class Db {
   }
   getPool() {
 
-    const pool = this.mysql.createPool({
+    const pool = this.mysql.createPool(process.env.JAWSDB_URL)
+
+    /* const pool = this.mysql.createPool({
       connectionLimit: 10,
       host: process.env.HOSTP,
       user: process.env.USERNAMEP,
       password: process.env.PASSWORDP,
       database: process.env.DATABASEP,
-    });
+    }); */
 
     return pool
   }
