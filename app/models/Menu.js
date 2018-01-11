14f0c5e8d117637b86b51240d226b8188cc5f152
RemyMozelle <timoz974@hotmail.fr>
2018-01-11 07:22:49 +0400

    change request

--- app/models/Menu.js
+++ app/models/Menu.js
@@ -5,9 +5,13 @@ class Menu {
 
   getMenu() {
     return new Promise((resolve, reject) => {
-      this.db.getPool().query('SELECT name, url FROM menus', (error, results, fields) => {
-        error ? reject(error) : resolve(results)
-      });
+      this.db.getPool().getConnection((err, connection) => {
+        if(err) throw err
+        connection().query('SELECT name, url FROM menus', (error, results, fields) => {
+          connection.release();
+          error ? reject(error) : resolve(results)
+        });
+      })
     });
   }
 }
