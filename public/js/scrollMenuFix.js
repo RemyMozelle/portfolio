3b530b92ea3eb71d0b9367a5c7ea1cbaa9f5ece3
RemyMozelle <timoz974@hotmail.fr>
2018-01-17 10:05:59 +0400

    fonctionne nickel

--- public/js/scrollMenuFix.js
+++ public/js/scrollMenuFix.js
@@ -4,18 +4,19 @@
 let navbar = document.querySelector('.navbar-default');
 let navbarBrand = document.querySelector('.nav-brand');
 let header = document.querySelector('header');
+let minimoi = document.querySelector('#minimoi');
 
+console.log(minimoi);
 window.addEventListener('scroll', (e) => {
 
   if (window.scrollY >= 200) {
-    navbar.classList.add('navbar-fixed-top');
+    //navbar.classList.add('navbar-fixed-top');
     navbar.setAttribute('style', 'background-color: rgba(40, 44, 52, 1');
-    navbarBrand.setAttribute('style', 'display: block');
+    minimoi.setAttribute('style', 'hidden');
   }
   else {
     navbar.classList.remove('navbar-fixed-top');
-    navbar.setAttribute('style', '');
-    navbarBrand.setAttribute('style', 'display: none');
+    minimoi.setAttribute('style', 'visible');
   }
 
 })
